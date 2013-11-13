fdb975234d88c394d9082f9e009eb2c6a9660643
Slavik Meltser <slavikme@gmail.com>
2013-11-13 10:36:07 +0200

    Added database and db user creation

--- includes/db.php
+++ includes/db.php
@@ -8,7 +8,7 @@ if ( basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME']) ) {
 
 require_once __DIR__ . './config.php';
 
-$db = new mysqli($cfg["db"]["hostname"], $cfg["db"]["username"], $cfg["db"]["password"], $cfg["db"]["database"], $cfg["db"]["port"]);
+$db = new mysqli($cfg["db"]["hostname"], $cfg["db"]["username"]."as", $cfg["db"]["password"], $cfg["db"]["database"], $cfg["db"]["port"]);
 if ( $db->connect_errno ) {
     try {
         if ( db_is_access_denied() ) {
--- includes/functions/db_functions.php
+++ includes/functions/db_functions.php
@@ -9,48 +9,60 @@ function db_is_access_denied($_db = NULL) {
     return in_array($db->connect_errno, array(1044,1045));
 }
 
-function initial_db() {
-    db_create_database();
-    db_create_user();
-    db_grand_user_privileges();
-    db_create_tables();
-    db_insert_init_data();
-}
-
-function db_create_database() {
+function db_get_root_connection() {
     global $cfg;
     $root_pw_tryouts = array("","mysql","root");
-    $db = new mysqli($cfg["db"]["hostname"], "root", "mysql", null, $cfg["db"]["port"]);
-    if ( db_is_access_denied($db) ) {
-        
+    foreach ( $root_pw_tryouts as $password ) {
+        $db = @new mysqli($cfg["db"]["hostname"], "root", $password, null, $cfg["db"]["port"]);
+        if ( !db_is_access_denied($db) ) {
+            return $db;
+        }
     }
+    return false;
+}
+
+function initial_db() {
+    if ( !( $root = db_get_root_connection() ) ) {
+        return false;
+    }
+    if ( db_create_database($root) && db_create_user($root) && db_grand_user_privileges($root)
+         && db_create_tables() && db_insert_init_data() ) {
+        return true;
+    }
+    return false;
+}
+
+function db_create_database($db) {
+    global $cfg;
     $res = $db->query("CREATE DATABASE {$cfg["db"]["database"]} "
              . "CHARACTER SET utf8 "
              . "COLLATE utf8_general_ci");
-    var_dump(array(
-        "result" => $res,
-        "errno" => $db->connect_errno,
-        "error" => $db->connect_error,
-    ));
+    return true;
 }
 
-function db_create_user() {
-    global $cfg, $db;
-    
+function db_create_user($db) {
+    global $cfg;
+    $res = $db->query("CREATE USER '{$cfg["db"]["username"]}'@'{$cfg["db"]["hostname"]}' "
+            . "IDENTIFIED BY '{$cfg["db"]["password"]}'");
+    return true;
 }
 
-function db_grand_user_privileges() {
-    global $cfg, $db;
-    
-    //FLUSH PRIVILEGES;
+function db_grand_user_privileges($db) {
+    global $cfg;
+    $res = $db->query("GRANT ALL PRIVILEGES "
+            . "ON {$cfg["db"]["database"]} . * "
+            . "TO  '{$cfg["db"]["username"]}'@'{$cfg["db"]["hostname"]}' "
+            . "WITH GRANT OPTION");
+    $db->query("FLUSH PRIVILEGES");
+    return true;
 }
 
 function db_create_tables() {
     global $cfg, $db;
-    
+    return true;
 }
 
 function db_insert_init_data() {
     global $cfg, $db;
-    
+    return true;
 }
